cbc34ca509d6b50411969cb80f14b0d99d6a4d1d
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-03-07 20:10:15 +0100

    Update Rozhled.java

--- Rozhled.java
+++ Rozhled.java
@@ -15,7 +15,7 @@ public class Rozhled{
         for(int i = -5; i < 6; i++){
             rozhled[1] = -6;
             rozhled[0] = rozhled[0] + 1;
-            for(int o = 0; o < 6; o++){
+            for(int o = 0; o < 11; o++){
                 rozhled[1] = rozhled[1] + 1;
             }
         }
